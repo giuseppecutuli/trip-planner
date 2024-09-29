44cbf817445e1be6789edd219acb0237362d42ca
Giuseppe Cutuli <cutuligiuseppe95@gmail.com>
2024-09-29 22:54:13 +0200

    feat: password service test

--- src/core/auth/password.service.spec.ts
+++ src/core/auth/password.service.spec.ts
@@ -1,18 +1,32 @@
-import { Test, TestingModule } from '@nestjs/testing'
 import { PasswordService } from './password.service'
 
 describe('PasswordService', () => {
-  let service: PasswordService
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PasswordService],
-    }).compile()
-
-    service = module.get<PasswordService>(PasswordService)
+  it('should be defined', () => {
+    expect(PasswordService).toBeDefined()
   })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined()
+  describe('hashPassword', () => {
+    it('should hash a password', async () => {
+      const password = 'testPassword'
+      const hashedPassword = await PasswordService.hashPassword(password)
+      expect(hashedPassword).not.toEqual(password)
+      expect(hashedPassword).toMatch(/^\$2[ayb]\$.{56}$/) // bcrypt hash format
+    })
+  })
+
+  describe('comparePassword', () => {
+    it('should return true for a valid password', async () => {
+      const password = 'testPassword'
+      const hashedPassword = await PasswordService.hashPassword(password)
+      const isMatch = await PasswordService.validatePassword(password, hashedPassword)
+      expect(isMatch).toBe(true)
+    })
+
+    it('should return false for an invalid password', async () => {
+      const password = 'testPassword'
+      const hashedPassword = await PasswordService.hashPassword(password)
+      const isMatch = await PasswordService.validatePassword('wrongPassword', hashedPassword)
+      expect(isMatch).toBe(false)
+    })
   })
 })
