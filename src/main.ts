6d167e355f0e6709eb5ce9c38708bf562f0e587b
Giuseppe Cutuli <cutuligiuseppe95@gmail.com>
2024-09-30 23:07:14 +0200

    chore: enable bearer on swagger

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,7 @@ async function bootstrap() {
       .setTitle(swaggerConfig.title)
       .setDescription(swaggerConfig.description)
       .setVersion(swaggerConfig.version)
+      .addBearerAuth()
       .build()
     const document = SwaggerModule.createDocument(app, options)
 
